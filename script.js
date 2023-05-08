2ca157cd5bf3ecc317df5c1c21882bf2bb313c5d
paulthomas0824 <paulthomas0824@gmail.com>
2023-05-08 15:38:32 -0500

    Added more files and updated the entire website

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Get all navigation links
     const navLinks = document.querySelectorAll('.nav-link');
   
-    navLinks.forEach(link => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetSection = document.querySelector(link.getAttribute('href'));
+    // Add a click event listener to each navigation link
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener('click', (event) => {
+        // Prevent the default click behavior
+        event.preventDefault();
+  
+        // Get the target section ID from the clicked link's href attribute
+        const targetSectionId = event.target.getAttribute('href');
+  
+        // Get the target section element
+        const targetSection = document.querySelector(targetSectionId);
+  
+        // Scroll to the target section smoothly
         targetSection.scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  
+    // Get all PDF links
+    const pdfLinks = document.querySelectorAll('.pdf-link');
+  
+    // Get the PDF iframe element
+    const pdfIframe = document.getElementById('pdf-iframe');
+  
+    // Add a click event listener to each PDF link
+    pdfLinks.forEach((pdfLink) => {
+      pdfLink.addEventListener('click', (event) => {
+        // Prevent the default click behavior
+        event.preventDefault();
+        event.stopPropagation();
+  
+        // Get the target PDF file from the clicked link's data-file attribute
+        const targetPdfFile = event.currentTarget.getAttribute('data-file');
+  
+        // Set the iframe src to the target PDF file
+        pdfIframe.setAttribute('src', targetPdfFile);
+  
+        // Return false to ensure the event doesn't propagate further
+        return false;
       });
     });
   });
